954e55fc21afdd9859db5ee497d9728066cee94c
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-16 07:47:27 +0600

    introduction to conditionals if

---
+++ Module-8.2.js
@@ -0,0 +1,41 @@
+// todo:   Module no 8.2       topic: Introduction to Conditionals if else : 
+
+/**
+ *  if structure : 
+ *  if (condition){
+ *       output }
+ */
+
+if(true){
+    console.log('conditional statement is start ');
+}
+if(false){
+    console.log('hidden thakbe ');
+}
+
+if(20 > 10){
+    console.log('20 is bigger than 10');
+}
+
+let prise = 500;
+let budget = 400;
+
+if(prise > budget ){
+    console.log("i can't buy this product "  );
+}
+
+
+prise =400;
+
+if(prise === budget){
+    console.log('i can buy this product ');
+}
+
+
+if( 30 >= 29){
+    console.log('30 is biggest number ');
+}
+
+if( 30 <= 40){
+    console.log('40 is biggest');
+}
