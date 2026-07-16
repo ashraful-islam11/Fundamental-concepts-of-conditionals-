543c6a7525d67c65b7fb7a696e6104ec8719f4fb
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-16 06:24:44 +0600

    comaprison operator with variable

---
+++ Module-8.1.js
@@ -0,0 +1,44 @@
+// todo:  Module no - 8.1      topic: Compare variables and Comparison operator
+
+// conditional operators :  
+/**
+ *  1. bigger: greater than : < 
+ *  2. smaller: less than   : >
+ *  3. equal :              : == , ===
+ * 
+ *  4. not equal            : != , !== 
+ *  5. greater than or equal: <= 
+ *  6. less than or equal   :   >= 
+ *  
+ *  7. and                  : && 
+ *  8. or                   : || 
+ * 
+ */
+
+// comparison start with  variable . answer is allows boolean data :
+
+let whiteShirt = 500;
+let blueShirt  = 450;
+
+//1. greater than : 
+console.log(whiteShirt >blueShirt);
+//2. less than :
+console.log(whiteShirt < blueShirt);
+
+// 3. equal : 
+console.log(whiteShirt == blueShirt);
+
+// 4. triple equal :
+console.log(whiteShirt === blueShirt);
+
+// 5. greater or equal : 
+console.log(whiteShirt >= blueShirt);
+
+// 6. less than or equal :
+console.log(whiteShirt <= blueShirt);
+
+// 7. not equal:
+console.log(whiteShirt != blueShirt);
+
+// 8. not double equal:
+console.log(whiteShirt !== blueShirt); 
