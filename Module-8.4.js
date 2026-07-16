a90b5e4df071c87411c7bb76c863336a5d7adfcf
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-16 10:47:16 +0600

    Conditional Branching If Else Condition

---
+++ Module-8.4.js
@@ -0,0 +1,31 @@
+// todo:   Module no - 8.4         topic:  Conditional Branching If Else Condition
+
+const prise = 500; 
+if( prise <= 200){
+    console.log('prise is very heigh');
+}else{
+    console.log('prise is not heigh');
+}
+
+
+const age = 20;
+if( age > 18){
+    console.log('he is adult');
+}else{
+    console.log('he is not adult');
+}
+
+let lamin = 18;
+if( lamin >= 18){
+    console.log('lamin ei bar world cup jitte pare');
+}else{
+    console.log('messi o jitte pare. ');
+}
+
+
+let neymar = 'come back';
+if( neymar === true){
+    console.log('i will see the next world cup');
+}else{
+    console.log('i will not see the next world cup ');
+}
